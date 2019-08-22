90fdca9e5acf31e2787f37dd85381b8ddfc8b40e
mashimeng <jphunt@163.com>
2019-08-22 16:30:47 +0800

    element-ui installed

--- src/router.js
+++ src/router.js
@@ -21,6 +21,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/demo",
+      name: "test1",
+      component: () => import("./views/demo/Test1.vue")
     }
   ]
 });
---
+++ src/views/demo/Test1.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <el-table
+      :data="[
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }
+      ]"
+      style="width: 100%"
+    >
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test1"
+};
+</script>
+
+<style scoped></style>
